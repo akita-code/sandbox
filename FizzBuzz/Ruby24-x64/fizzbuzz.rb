017fb7fddd04a3f8a7ac8e3135ad7b858110db8d
akita-code <user@a2script.jp>
2018-07-09 04:24:35 +0900

    修正

--- FizzBuzz/Ruby24-x64/fizzbuzz.rb
+++ FizzBuzz/Ruby24-x64/fizzbuzz.rb
@@ -1,5 +1,5 @@
 fx =-> (i) {
-  case
+  puts case
   when i % 15 == 0 then "FizzBuzz!"
   when i % 3 == 0 then "Fizz"
   when i % 5 == 0 then "Buzz"
@@ -7,4 +7,4 @@ fx =-> (i) {
   end
 }
 
-1.upto(100).map{|s| puts fx.(s)}
+1.upto(100).map(&fx)
