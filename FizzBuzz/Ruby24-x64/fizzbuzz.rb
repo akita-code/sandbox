9d160f41dcb6aa4e3419da7c4e6f3d141eea1ae7
akita-code <user@a2script.jp>
2018-07-09 04:11:16 +0900

    修正

--- FizzBuzz/Ruby24-x64/fizzbuzz.rb
+++ FizzBuzz/Ruby24-x64/fizzbuzz.rb
@@ -1,8 +1,14 @@
-1.upto(100) { |i|
-  puts case
+fx =-> (i) {
+  case
   when i % 15 == 0 then "FizzBuzz!"
   when i % 3 == 0 then "Fizz"
   when i % 5 == 0 then "Buzz"
   else ""
   end
 }
+
+num_arr = 1.upto(100).to_a
+
+num_arr.each do |i|
+  puts fx.(i)
+end
