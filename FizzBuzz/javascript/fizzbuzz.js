e6bcebefa2d2e868a39cb4b3a6c3dd5ccb9e305a
akita-code <user@a2script.jp>
2018-07-10 01:22:32 +0900

    修正

--- FizzBuzz/javascript/fizzbuzz.js
+++ FizzBuzz/javascript/fizzbuzz.js
@@ -1,17 +1,24 @@
-var APP = APP || {};
+var APP = APP || {
+    util:{},
+};
 
-APP.arr = Array.apply(null, new Array(100)).map((v, i) => i+1);
+APP.util = (function() {
+    fizzbuzz = i => {
+        if (i % 15 === 0) {
+            return "FizzBuzz!";
+        } else if (i % 3 === 0) {
+            return "Fizz";
+        } else if (i % 5 === 0) {
+            return "Buzz";
+        } else {
+            return i.toString(10);
+        }
+    };
 
-APP.fizzbuzz = i => {
-    if (i % 15 === 0) {
-        return "FizzBuzz!";
-    } else if (i % 3 === 0) {
-        return "Fizz";
-    } else if (i % 5 === 0) {
-        return "Buzz";
-    } else {
-        return i.toString(10);
-    }
-}
+    return {
+        arr : Array.apply(null, new Array(100)).map((v, i) => i+1),
+        fizzbuzz: fizzbuzz,
+    };
+}());
 
-APP.arr.map(APP.fizzbuzz).forEach(console.log);
+APP.util.arr.map(APP.util.fizzbuzz).forEach(console.log);
