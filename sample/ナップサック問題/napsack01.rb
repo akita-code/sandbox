38bf4dde0aba0d97d060797ee3e2da56c25a8f51
akita-code <user@a2script.jp>
2018-07-18 22:57:20 +0900

    修正

--- sample/ナップサック問題/napsack01.rb
+++ sample/ナップサック問題/napsack01.rb
@@ -6,7 +6,13 @@ init =-> () {
     w, n = gets.to_s.split(" ").map(&:to_i)
     item_list = []
     n.times do |l|
-        item_list << gets.to_s.split(" ").map(&:to_i)
+        item_list << gets.to_s.split(" ").map.with_index do |item, i|
+            case i
+            when NAME;  item
+            when PRICE; item.to_i
+            when WAIT;  item.to_i
+            end
+        end
     end
     [w, n, item_list]
 }
@@ -42,11 +48,28 @@ put_memo_line =-> (i) {
             end
         end
     end
-    p next_line
 }
 
+# メモ配列の書き込み
 N.times do |i|
     put_memo_line[i]
 end
 
-p memo
+get_use_item_list =-> () {
+    use_item_list = []
+    index = 0
+
+    # 最大値の位置を取得
+    memo.last.each_with_index{|n, i| index = i if n == memo.last.max }
+
+    # リストを遡って使用したアイテムを特定
+    (N-1).downto(0) do |i|
+        if memo[i][index] != memo[i-1][index]
+            use_item_list << item_list[i]
+            index -= item_list[i][WAIT]
+        end
+    end
+    use_item_list.reverse
+}
+
+p get_use_item_list.()
