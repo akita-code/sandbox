cb4704fe27e5532acf178a4de5af8e214c1b8132
akita-code <user@a2script.jp>
2018-07-23 03:37:12 +0900

    追加

---
+++ Javaデザインパターン/designpattern/src/designpattern/builder/TextBuilder.java
@@ -0,0 +1,36 @@
+package designpattern.builder;
+
+public class TextBuilder extends Builder {
+    private StringBuilder buffer = new StringBuilder();
+
+    @Override
+    public void makeTitle(String title) {
+	buffer.append("==============================\n");
+	buffer.append("[" + title + "]\n");
+	buffer.append("\n");
+    }
+
+    @Override
+    public void makeString(String str) {
+	buffer.append("■" + str + "\n");
+	buffer.append("\n");
+    }
+
+    @Override
+    public void makeItems(String[] items) {
+	for (String s : items) {
+	    buffer.append(" ・" + s + "\n");
+	}
+	buffer.append("\n");
+    }
+
+    @Override
+    public void close() {
+	buffer.append("==============================\n");
+    }
+
+    public String getResult() {
+	return buffer.toString();
+    }
+
+}
